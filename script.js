fdd6d739d3232db415399ab397f63271f7223e00
Prathmesh Yelne <88598499+prathmeshyelne@users.noreply.github.com>
2021-12-31 12:37:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 /**
  * Js event handler to change between Clark and Superman modes.
  *
- * @author Ezequiel Calvo <ezecafre@gmail.com> 
- *                        Follow me @EzequielCalvo
- *                        Hashtag #CSSDrawing #Minion
  */
 
 $('.btn').on('click', function() {
